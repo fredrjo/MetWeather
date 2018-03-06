c9e383f557850e286bce950c7d6e66142f3ebab6
Fredrik Johnsen <fredrik.johnsen@cebyc.no>
2018-03-06 11:13:09 +0100

    flasking

---
+++ models/MeasurementModel.py
@@ -0,0 +1,6 @@
+class MeasurementModel:
+
+    def __init__(dateTime, mType, value):
+        self.dateTime = dateTime
+        self.mType = mType
+        self.value = value
---
+++ models/StationModel.py
@@ -0,0 +1,8 @@
+class StationModel:
+
+    def __init__(name, longitude, latitude, masl, operational):
+        self.name = name
+        self.longitude = longitude
+        self.latitude = latitude
+        self.masl = masl
+        self.operational = operational
