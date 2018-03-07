1b681679e1ecb1bfeeb11ba669f2f4e171d039c6
Fredrik Johnsen <fredrik.johnsen@cebyc.no>
2018-03-07 16:00:37 +0100

    home

--- models/MeasurementModel.py
+++ models/MeasurementModel.py
@@ -1,6 +1,12 @@
-class MeasurementModel:
+from db import db
 
-    def __init__(dateTime, mType, value):
+class MeasurementModel(db.Model):
+    __tablename__ = 'measurements'
+    id = db.Column(db.Integer, primary_key = True)
+    mType = db.Column(db.String(255))
+    value = db.Column(db.Float(precision = 2))
+
+    def __init__(self, dateTime, mType, value):
         self.dateTime = dateTime
         self.mType = mType
         self.value = value
--- models/StationModel.py
+++ models/StationModel.py
@@ -1,13 +1,39 @@
-from MetWrapper import MetWrapper
-class StationModel:
+import sqlite3
+import json
+from db import db
 
-    def __init__(name, longitude, latitude, masl, operational):
+from MetWrapper import MetWrapper
+class StationModel(db.Model):
+    __tablename__ = 'stations'
+
+    id = db.Column(db.String, primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __init__(self, id, name): #, longitude, latitude, masl, operational):
+        self.id = id
         self.name = name
-        self.longitude = longitude
-        self.latitude = latitude
-        self.masl = masl
-        self.operational = operational
+        #self.longitude = longitude
+        #self.latitude = latitude
+        #self.masl = masl
+        #self.operational = operational
+
+    def findByName(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def getAllStations(cls):
+        stations = []
+        getThemBoiz = StationModel.query.all()
+        for st in getThemBoiz:
+            stations.append({'id' : st.id, 'name' : st.name})
+        return stations
+
 
     def getStationsFromMet():
         stuff = MetWrapper.getStationsFromMet('NO')
-        return stuff.json()
+        #print(stuff)
+        for item in stuff['data']:
+            print(item)
+            newStation = StationModel(item['id'], item['name'])
+            db.session.add(newStation)
+        db.session.commit()
+#        print('ok')
